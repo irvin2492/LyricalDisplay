ae21674e5251a94bacddded87f640117702bcfd9
Irvin Samuel <irvinsamuels@gmail.com>
2020-12-24 23:57:43 -0800

    Updated refresh functionality and added support for no current song playback

--- lyricaldisplay.py
+++ lyricaldisplay.py
@@ -21,21 +21,39 @@ class ApiData():
                                 username = self.user))
 
         #Collects track data from Spotify API and uses that to get lyrics from Genius API
+
+        self.__current_track_data = self.sp.current_user_playing_track()
         try:
-            self.__current_track_data = self.sp.current_user_playing_track()
             self.__current_track_name = self.__current_track_data["item"]["name"]
             self.__current_track_artist = self.__current_track_data["item"]["artists"][0]["name"]
+            self.__current_track_duration = self.__current_track_data["item"]["duration_ms"]
+
             __current_track_lyricdata= self.gen.search_song(self.__current_track_name,self.__current_track_artist)
             self.__current_track_lyrics = __current_track_lyricdata.lyrics
         except:
+            self.__current_track_name = None
+            self.__current_track_artist = None
+            __current_track_lyricdata = None
+            self.__current_track_lyrics = None
+            self.__current_track_duration = None
             print("No song playing.")
 
     def update_song(self):
         self.__current_track_data = self.sp.current_user_playing_track()
-        self.__current_track_name = self.__current_track_data["item"]["name"]
-        self.__current_track_artist = self.__current_track_data["item"]["artists"][0]["name"]
-        __current_track_lyricdata= self.gen.search_song(self.__current_track_name,self.__current_track_artist)
-        self.__current_track_lyrics = __current_track_lyricdata.lyrics
+        try:
+            self.__current_track_name = self.__current_track_data["item"]["name"]
+            self.__current_track_artist = self.__current_track_data["item"]["artists"][0]["name"]
+            self.__current_track_duration = self.__current_track_data["item"]["duration_ms"]
+
+            __current_track_lyricdata= self.gen.search_song(self.__current_track_name,self.__current_track_artist)
+            self.__current_track_lyrics = __current_track_lyricdata.lyrics
+
+        except:
+            self.__current_track_name = None
+            self.__current_track_artist = None
+            __current_track_lyricdata = None
+            self.__current_track_lyrics = None
+            print("No song playing.")
 
     @property #testing only
     def track_data(self):
@@ -50,40 +68,55 @@ class ApiData():
     @property
     def track_lyrics(self):
         return self.__current_track_lyrics
+    @property
+    def track_duration(self):
+        return self.__current_track_duration
 
 class gui():
 
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry("500x500")
+        self.root.geometry("800x500")
         self.root.title("LyricalDisplay")
         self.test = ApiData('--rzqdJGFcVjzUa4sLjvL3ADoT9Bj9bvWFWQCiTut7m41yIH8QVdBetvyDnGWe3n')
 
         headFont = Font(family = "Helvetica", size = 18)
         bodyFont = Font(family = "Helvetica", size = 12)
 
-        self.track_label = tk.Label(self.root, text = "Currently playing: '" + self.test.track_name + "' by "+ self.test.track_artist, font = headFont)
+        if(self.test.track_name is None or self.test.track_artist is None):
+            self.track_label = tk.Label(self.root, text = "No song currently playing.")
+        else:
+            self.track_label = tk.Label(self.root, text = "Currently playing: '" + self.test.track_name + "' by "+ self.test.track_artist, font = headFont)
         self.track_label.pack()
 
-        self.start_button = tk.Button(self.root,text = "Start!", command = lambda: print(self.test.track_lyrics))
-        self.start_button.pack()
+        self.refresh_button = tk.Button(self.root,text = "refresh?", command = self.update_gui)
+        self.refresh_button.pack() #testing
 
-        self.start_button2 = tk.Button(self.root,text = "refresh?", command = self.update_gui)
-        self.start_button2.pack() #testing
+        if(self.test.track_lyrics is None):
+            self.lyrics_box = tk.Text(self.root, height = 50, width = 50, font = bodyFont)
+            self.lyrics_box.config(state = 'disabled')
+            self.lyrics_box.pack()
 
-        self.lyrics_box = tk.Text(self.root, height = 50, width = 50, font = bodyFont)
-        self.lyrics_box.insert(tk.END,self.test.track_lyrics)
-        self.lyrics_box.config(state = 'disabled')
+        else:
+            self.lyrics_box = tk.Text(self.root, height = 50, width = 50, font = bodyFont)
+            self.lyrics_box.insert(tk.END,self.test.track_lyrics)
+            self.lyrics_box.config(state = 'disabled')
 
-        self.lyrics_box.pack()
+            self.lyrics_box.pack()
 
     def update_gui(self):
         self.test.update_song()
         self.lyrics_box.config(state = 'normal')
-        self.track_label.config(text = "Currently playing: '" + self.test.track_name + "' by "+ self.test.track_artist)
-        self.lyrics_box.delete(1.0,tk.END)
-        self.lyrics_box.insert(tk.END,self.test.track_lyrics)
-        self.lyrics_box.config(state = 'disabled')
+        if(self.test.track_name is None or self.test.track_artist is None):
+            self.track_label.config(text = "No song currently playing.")
+            self.lyrics_box.delete(1.0,tk.END)
+            self.lyrics_box.insert(tk.END,"")
+            self.lyrics_box.config(state = 'disabled')
+        else:
+            self.track_label.config(text = "Currently playing: '" + self.test.track_name + "' by "+ self.test.track_artist)
+            self.lyrics_box.delete(1.0,tk.END)
+            self.lyrics_box.insert(tk.END,self.test.track_lyrics)
+            self.lyrics_box.config(state = 'disabled')
 
     def run_loop(self):
         self.root.mainloop()
